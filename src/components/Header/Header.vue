d08a14d066056dfce74d216c80bd81d75790b0c5
Bhuvan Mishra <bhuvan.mishra@clecotech.com>
2019-08-17 12:09:38 +0530

    Integrated routing via link.

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      <div id="header" class="d-flex align-items-center justify-content-center bg-primary">Welcome to the application!</div>
+    <div id="header" class="text-center bg-primary">
+      <router-link class="homePageLink" to="/">HomePage.</router-link>
+      <div>Welcome to the application!</div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +12,12 @@
 
 <style scoped>
 #header {
-    min-height: 100px;
-    color: white;
+  min-height: 100px;
+  color: white;
+}
+.homePageLink {
+  color:yellow;
+  position: absolute;
+  left: 0;
 }
 </style>
